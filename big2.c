5aa312192457c42802a604ced968250c3cc469d9
Cloud User <ec2-user@ip-172-31-26-165.eu-north-1.compute.internal>
2026-01-04 11:31:22 +0000

    project

---
+++ big2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void biggest2() {
+    int a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    if (a > b) {
+        printf("%d is the biggest\n", a);
+    } else if (b > a) {
+        printf("%d is the biggest\n", b);
+    } else {
+        printf("Both numbers are equal\n");
+    }
+
+   // return 1;
+}
+
